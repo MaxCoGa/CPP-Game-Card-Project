fb9441c72c9c21386a5c696332885ca735ba9c5f
Maxime <mcote074@uottawa.ca>
2018-11-16 14:05:30 -0500

    Update Reward.cpp

--- sources/Reward.cpp
+++ sources/Reward.cpp
@@ -1,11 +1,15 @@
 #include "Reward.h"
 #include <iostream>
 
-Reward::Reward() {
-	
+Reward::Reward(int number) {
+	if(number<=4 && number >=1){
+		_number=number;
+	}else{
+		_number  =1;
+	}
 }
 
-std::ostream &operator << (std::ostream &out, const Reward &r) {
-	
-	return out;
+std::ostream &operator << (std::ostream &os, const Reward &r) {
+	os << r._number;
+	return os;
 }
