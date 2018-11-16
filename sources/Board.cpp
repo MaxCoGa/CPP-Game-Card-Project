14bf9aba46024b81ffbc75a1cced54ed32ae7225
Maxime <mcote074@uottawa.ca>
2018-11-16 15:36:01 -0500

    Update Board.cpp

--- sources/Board.cpp
+++ sources/Board.cpp
@@ -1,3 +1,22 @@
 #include "Board.h"
 
+Board::~Board() {
+
+}
+
+bool Board::isFaceUp(const Letter&, const Number&) const {
+    return true;
+}
+
+bool Board::turnFaceUp(const Letter&, const Number&) {
+    return true;
+}
+
+bool Board::turnFaceDown(const Letter&, const Number&) {
+    return true;
+}
+
+void Board::reset() {
+
+}
 
