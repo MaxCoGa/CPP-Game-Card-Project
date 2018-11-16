19d9f98471a05eb2e2667c3469d58a7165a3b298
Maxime <mcote074@uottawa.ca>
2018-11-16 13:52:31 -0500

    Update Card.h

--- headers/Card.h
+++ headers/Card.h
@@ -7,22 +7,47 @@ enum FaceAnimal { Crab, Penguin, Octopus, Walrus, Turtle };
 enum FaceBackground { Red, Green, Purple, Blue, Yellow };
 
 class Card {
+	friend class Deck;
 private:
-	FaceAnimal animal;
-	FaceBackground background;
-	int nRow = 3;
-	char face, colour;
+	FaceAnimal _animal;
+	FaceBackground _background;
+	//int nRow = 3;
+	//char face, colour;
 
-	Card(char face_animal, char colour_background);
+	Card(FaceAnimal animal, FaceBackground background):
+		_animal(animal), _background(background)
+		{}//constructeur privee
 
 public:
-	string row(int row);
-	int getNRows() const { return nRow; }
+	//string row(int row);
+	//int getNRows() const { return nRow; }
 
-	void setCharAnimal(FaceAnimal);
-	void setCharBackground(FaceBackground);
+	void getAnimal(FaceAnimal) const;//inline au lieu de void?
+	void getBackground(FaceBackground) const;//inline au lieu de void?
 	
-	void print();
+	//CardDeck en a besoins
+	Card(const Card& c) = delete; //ne peut pas être copié 
+	Card& operator= (const Card& c) = delete; //ni assigné
+	//
+
+	~Card() {}//destructeur
+
+	inline int getNRows() const { //trois rangee
+		return 3;	
+	}
+
+	string operator() (const int row) const;
+
+
+	operator FaceAnimal() const { 
+		return _animal;
+	}
+
+	operator FaceBackground() const { 
+		return _background; 
+	}
+
+	//void print();
 };
 
 #endif 
