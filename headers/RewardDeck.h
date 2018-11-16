ca52590042ebf854908ee27db05fbf2c417c7ff0
Maxime <mcote074@uottawa.ca>
2018-11-16 14:28:08 -0500

    Update RewardDeck.h

--- headers/RewardDeck.h
+++ headers/RewardDeck.h
@@ -1,10 +1,23 @@
 #ifndef REWARDDECK_H
 #define REWARDDECK_H
 
+#define NUM1RUBIES (4)
+#define NUM2RUBIES (3)
+#define NUM3RUBIES (2)
+#define NUM4RUBIES (1)
+
+#include "reward.h"
+#include "deck.h"
+#include <vector>
+#include <iterator>
+#include <iostream>
+
 class RewardDeck : public Deck<Reward>{//meme structure que CardDeck juste avec les noms Reward au lieu de Card
     private:
-        static vector<Reward*> deck;
+        static std::vector<Reward*> deck;
 	    int lastCard = -1;
+
+
         RewardDeck() {};
     public:
         ~RewardDeck();
