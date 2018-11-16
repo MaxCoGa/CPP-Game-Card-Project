6bd985698a52cb17406a8de1b5d5b6b7b103a363
Maxime <mcote074@uottawa.ca>
2018-11-16 14:14:01 -0500

    Update Deck.h

--- headers/Deck.h
+++ headers/Deck.h
@@ -2,7 +2,8 @@
 #define DECK_H
 template <class C> class Deck{
 public:
-	virtual ~Deck() {};
+	virtual ~Deck() {};//destructor a ne pas inlucre dans les derives
+
 	virtual void shuffle() = 0; // Vous devez vous servir de la fonction std::random_shuffle de la bibliothèque standard. 
 	virtual C* getNext() = 0;
 	virtual bool isEmpty() const = 0;
