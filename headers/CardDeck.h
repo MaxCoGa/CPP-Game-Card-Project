23ce0f978c2936d97c8ac7225e62e115a477eea6
Maxime <mcote074@uottawa.ca>
2018-11-16 14:19:04 -0500

    Update CardDeck.h

---
+++ headers/CardDeck.h
@@ -0,0 +1,23 @@
+#ifndef CARDDECK_H
+#define CARDDECK_H
+
+#include "deck.h"
+#include "card.h"
+#include <vector>
+
+class CardDeck : public Deck<Card>{
+    private:
+        static std::vector<Card*> deck;
+        int lastCard = -1;
+        CardDeck(){};
+    public:
+        CardDeck(const CardDeck& cd) = delete;
+        CardDeck& operator= (const CardDeck& cd) = delete;
+        static CardDeck& make_CardDeck();
+
+        virtual void shuffle();
+        virtual Card* getNext();
+        virtual bool isEmpty() const;
+};
+
+#endif
