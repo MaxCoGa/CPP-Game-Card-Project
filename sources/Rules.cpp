3a88fd40778e46d5f96c453e85e3d94e3d005739
Maxime <mcote074@uottawa.ca>
2018-11-23 15:06:02 -0500

    Create Rules.cpp

---
+++ sources/Rules.cpp
@@ -0,0 +1,34 @@
+#include "rules.h"
+
+
+
+Rules::Rules(){}
+
+	
+
+
+Rules::~Rules(){}
+
+bool Rules::isValid(const Game& g) {
+	
+}
+
+bool Rules::gameOver(const Game& g) {
+	
+}
+
+bool Rules::roundOver(const Game& g) {
+	
+}
+
+const Player& Rules::getNextPlayer(const Game& g) {
+	
+}
+
+Side Rules::nextPlayer() {
+	
+}
+
+Side Rules::previousPlayer() {
+	
+}
