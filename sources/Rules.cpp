b5e5a23a5b7f7b62bb5385f738bff51eb87cbf46
valentinmagot <valentin_magot@outlook.com>
2018-11-28 17:16:29 -0500

    Rules mini update

--- sources/Rules.cpp
+++ sources/Rules.cpp
@@ -1,4 +1,4 @@
-#include "rules.h"
+#include "Rules.h"
 
 
 
@@ -10,25 +10,34 @@ Rules::Rules(){}
 Rules::~Rules(){}
 
 bool Rules::isValid(const Game& g) {
-	
+    return g._currentcard == g._previouscard;
 }
 
 bool Rules::gameOver(const Game& g) {
-	
+    if(g._round == 7){
+        return true;
+    }else{
+        return false;
+    }
 }
 
 bool Rules::roundOver(const Game& g) {
-	
+    if(g._player.size() == 1){
+        return true;
+    }else{
+        return false;
+    }
 }
 
 const Player& Rules::getNextPlayer(const Game& g) {
-	
+    
+    
 }
 
 Side Rules::nextPlayer() {
-	
+    
 }
 
 Side Rules::previousPlayer() {
-	
+    
 }
