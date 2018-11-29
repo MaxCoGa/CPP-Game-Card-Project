787ed228005863671ace2c57b0b5542aecbc47f4
Maxime <mcote074@uottawa.ca>
2018-11-29 11:37:42 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -52,19 +52,25 @@ int main(){
 
 //Game
 #if main
-	//set Board, Game and Rules to nullptr
+	//ADD counter for number of game win or loss it may be an array/vector of win or loss
+
+	//major definitions
 	Board* board = nullptr;
 	Game* game = nullptr;
 	Rules* rules = nullptr;
+
+	//TESTING COLOR
+	int color = 2;
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 2);
+
 
 	//game input
 	int input;
 	int help;
 	int display;//normal or expert
 	int rule;//normal or expert
-
-	cout << "Memoarr, the board game. Implemented by Maxime & Valentin\n" << endl;	
-	cout << "Press any key to START the game setup or 1 to how to play" << endl;
+	cout.width(100); cout << "Memoarr, the board game. Implemented by Maxime & Valentin\n" << endl;
+	cout.width(99); cout << "Press any key to START the game setup or 1 to how to play" << endl;
 	while (true) {
 		cin >> help;
 		if (!cin.fail() && help == 1){
@@ -82,8 +88,27 @@ int main(){
 		}
 	}
 	//cin.clear();
-	
+
 	cout << "This game have two displays: \n0 for normal \n1 for expert" << endl;
+	while (true) {
+		cin >> help;
+		if (!cin.fail() && (display == 0 || display ==1)){
+			cin.clear();
+			howToPlay();
+			break;
+		}
+		else if(cin.fail() && (display != 0 || display != 1)){ 
+			cin.clear();
+			cin;
+			cin.ignore(100, '\n');
+			cout << "\nSETUP" << endl;
+			break;
+			
+		}
+	}
+	//cin.clear();
+
+	
 #endif
 
     
